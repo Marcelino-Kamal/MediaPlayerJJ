d8710f52b7044fd08a3fd64d643df08d392c9b24
Marcelino <marconaoum@gmail.com>
2025-05-01 00:24:20 +0300

    Model

---
+++ src/main/java/com/mediaplayer/models/Song.java
@@ -0,0 +1,26 @@
+package com.mediaplayer.models;
+
+public class Song {
+    private String name;
+    private String path;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return this.path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    
+}
+
+    
